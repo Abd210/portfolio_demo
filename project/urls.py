310d5ea498d8fb5b8189facd1f19db7f0ade735d
Abd210 <150333656+Abd210@users.noreply.github.com>
2025-01-09 09:47:38 -0800

    done

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,16 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import  path
 
 from portfolio import settings
 from project import views
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
-    path('', views.projects_list), 
+    path('', views.home_page, name='home'),  
+    path('project/<int:project_id>/', views.project_detail, name='project_detail'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/views.py
+++ project/views.py
@@ -4,9 +4,14 @@ import project
 
 # Create your views here.
 from .models import project
+from django.shortcuts import render, get_object_or_404
+
+def project_detail(request, project_id):
+    project_obj = get_object_or_404(project, id=project_id)
+    return render(request, 'project_detail.html', {'project': project_obj})
 
 
 
-def projects_list(request):
-    projects = project.objects.all()
-    return render(request, 'projects_list.html', {'projects': projects})
+def home_page(request):
+    projects = project.objects.all()  # Fetch your projects
+    return render(request, 'home.html', {'projects': projects})
